199f706387fa8b17fb876b686c8acff5fc3280a7
Juliano Moser <juliano.moserjj@gmail.com>
2022-09-18 20:43:21 -0300

    Descrevendo o modelo de representação de problema e solucuinado o problema de serialização de OffsetDateTime

--- src/main/java/com/moser/moserfood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/moser/moserfood/core/openapi/SpringFoxConfig.java
@@ -1,5 +1,8 @@
 package com.moser.moserfood.core.openapi;
 
+import com.fasterxml.classmate.TypeResolver;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.moser.moserfood.api.exceptionhandler.Problem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,6 +18,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.JacksonModuleRegistrar;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
@@ -28,7 +32,14 @@ import java.util.List;
 public class SpringFoxConfig {
 
     @Bean
+    public JacksonModuleRegistrar springFoxJacksonConfig() {
+        return objectMapper -> objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    @Bean
     public Docket apiDocket() {
+        var typeResolver = new TypeResolver();
+
         return new Docket(DocumentationType.OAS_30)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.moser.moserfood.api"))
@@ -40,6 +51,7 @@ public class SpringFoxConfig {
                 .globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
                 .globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
+                .additionalModels(typeResolver.resolve(Problem.class))
                 .apiInfo(apiInfo())
                 .tags(tags()[0], tags());
     }

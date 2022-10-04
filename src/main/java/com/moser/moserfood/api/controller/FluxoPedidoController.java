18342bfcde355348d5580ef217e504de993bd170
Juliano Moser <juliano.moserjj@gmail.com>
2022-10-04 15:40:46 -0300

    Descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/com/moser/moserfood/api/controller/FluxoPedidoController.java
+++ src/main/java/com/moser/moserfood/api/controller/FluxoPedidoController.java
@@ -1,38 +1,34 @@
 package com.moser.moserfood.api.controller;
 
+import com.moser.moserfood.api.openapi.controller.FluxoPedidoControllerOpenApi;
 import com.moser.moserfood.domain.service.FluxoPedidoService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Juliano Moser
  */
-@Api(tags = "Order status")
 @RestController
-@RequestMapping("/pedidos/{codigoPedido}")
-public class FluxoPedidoController {
+@RequestMapping(path = "/pedidos/{codigoPedido}", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FluxoPedidoController implements FluxoPedidoControllerOpenApi {
 
     @Autowired
     private FluxoPedidoService fluxoPedidoService;
 
-    @ApiOperation("Aletrar um pedido para confirmado")
     @PutMapping("/confirmacao")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void confirmar(@PathVariable String codigoPedido) {
         fluxoPedidoService.confirmar(codigoPedido);
     }
 
-    @ApiOperation("Aletrar um pedido para entregue")
     @PutMapping("/entrega")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void entregar(@PathVariable String codigoPedido) {
         fluxoPedidoService.entregar(codigoPedido);
     }
 
-    @ApiOperation("Aletrar um pedido para cancelado")
     @PutMapping("/cancelamento")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void cancelar(@PathVariable String codigoPedido) {

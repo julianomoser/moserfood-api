1cd8a1a0d11f020238fbcb5f16454cddf9d6be09
Juliano Moser <juliano.moserjj@gmail.com>
2022-10-07 12:31:37 -0300

    Descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/moser/moserfood/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/moser/moserfood/api/controller/UsuarioGrupoController.java
@@ -2,12 +2,12 @@ package com.moser.moserfood.api.controller;
 
 import com.moser.moserfood.api.assembler.GrupoDTOAssembler;
 import com.moser.moserfood.api.model.GrupoDTO;
+import com.moser.moserfood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.moser.moserfood.domain.model.Usuario;
 import com.moser.moserfood.domain.service.UsuarioService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,10 +15,9 @@ import java.util.List;
 /**
  * @author Juliano Moser
  */
-@Api(tags = "User group")
 @RestController
-@RequestMapping("/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(path = "/usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private UsuarioService usuarioService;
@@ -26,22 +25,19 @@ public class UsuarioGrupoController {
     @Autowired
     private GrupoDTOAssembler grupoDTOAssembler;
 
-    @ApiOperation("Lista grupo de usuários")
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<GrupoDTO> listar(@PathVariable Long usuarioId) {
         Usuario usuario = usuarioService.findOrFail(usuarioId);
         return grupoDTOAssembler.toCollectionDTO(usuario.getGrupos());
     }
 
 
-    @ApiOperation("Associa usuários a um grupo")
-    @PutMapping("/{grupoId}")
+    @PutMapping(path = "/{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void associar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {
         usuarioService.associarGrupo(usuarioId, grupoId);
     }
 
-    @ApiOperation("Desassocia usuários de um grupo")
     @DeleteMapping("/{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desassociar(@PathVariable Long usuarioId, @PathVariable Long grupoId) {

0d64ef189e8152c0f4cebf092351f609c684f0de
Juliano Moser <juliano.moserjj@gmail.com>
2022-10-07 12:10:41 -0300

    Descrevendo documentação de endpoints de associação de permissões com grupos

--- src/main/java/com/moser/moserfood/api/controller/GrupoPermissaoController.java
+++ src/main/java/com/moser/moserfood/api/controller/GrupoPermissaoController.java
@@ -1,18 +1,14 @@
 package com.moser.moserfood.api.controller;
 
 import com.moser.moserfood.api.assembler.PermissaoDTOAssembler;
-import com.moser.moserfood.api.exceptionhandler.Problem;
 import com.moser.moserfood.api.model.PermissaoDTO;
+import com.moser.moserfood.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.moser.moserfood.domain.model.Grupo;
 import com.moser.moserfood.domain.service.GrupoService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,8 +18,8 @@ import java.util.List;
  */
 @Api(tags = "Group permission")
 @RestController
-@RequestMapping("/grupos/{grupoId}/permissoes")
-public class GrupoPermissaoController {
+@RequestMapping(path = "/grupos/{grupoId}/permissoes", produces = MediaType.APPLICATION_JSON_VALUE)
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
 
     @Autowired
     private GrupoService grupoService;
@@ -31,33 +27,22 @@ public class GrupoPermissaoController {
     @Autowired
     private PermissaoDTOAssembler permissaoModelAssembler;
 
-    @ApiOperation("Lista os grupos de permissões")
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<PermissaoDTO> listar(@PathVariable Long grupoId) {
         Grupo grupo = grupoService.findOrFail(grupoId);
 
         return permissaoModelAssembler.toCollectionDTO(grupo.getPermissoes());
     }
 
-    @ApiOperation("Associonar uma permissão a um gurpo por Id")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Grupo associado"),
-            @ApiResponse(responseCode = "404", description = "Grupo ou permissão não encontrada", content = @Content(schema =
-            @Schema(implementation = Problem.class)))  })
     @PutMapping("/{permissaoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void associar(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
         grupoService.associarPermissao(grupoId, permissaoId);
     }
 
-    @ApiOperation("Desassocionar uma permissão a um gurpo por Id")
-    @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "Grupo desassociado"),
-            @ApiResponse(responseCode = "404", description = "Grupo ou permissão não encontrada", content = @Content(schema =
-            @Schema(implementation = Problem.class)))  })
     @DeleteMapping("/{permissaoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void desaassociar(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
+    public void desassociar(@PathVariable Long grupoId, @PathVariable Long permissaoId) {
         grupoService.desassociarPermissao(grupoId, permissaoId);
     }
 }

19a6ea0852b37653688cc74657653817d5e32a19
Juliano Moser <juliano.moserjj@gmail.com>
2022-10-10 02:51:50 -0300

    Adicionando a URI do recurso criado no header da resposta

--- src/main/java/com/moser/moserfood/api/controller/CidadeController.java
+++ src/main/java/com/moser/moserfood/api/controller/CidadeController.java
@@ -1,5 +1,6 @@
 package com.moser.moserfood.api.controller;
 
+import com.moser.moserfood.api.ResourceUriHelper;
 import com.moser.moserfood.api.assembler.CidadeDTOAssembler;
 import com.moser.moserfood.api.assembler.CidadeInputDisassembler;
 import com.moser.moserfood.api.openapi.controller.CidadeControllerOpenApi;
@@ -11,11 +12,17 @@ import com.moser.moserfood.domain.model.Cidade;
 import com.moser.moserfood.domain.repository.CidadeRepository;
 import com.moser.moserfood.domain.service.CidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -59,7 +66,11 @@ public class CidadeController implements CidadeControllerOpenApi {
 
             cidade = cidadeService.salvar(cidade);
 
-            return cidadeModelAssembler.toDTO(cidade);
+            CidadeDTO cidadeDTO = cidadeModelAssembler.toDTO(cidade);
+
+            ResourceUriHelper.addUriInResponseHeader(cidadeDTO.getId());
+
+            return cidadeDTO;
         } catch (EstadoNaoEncontradoException e) {
             throw new NegocioException(e.getMessage(), e);
         }

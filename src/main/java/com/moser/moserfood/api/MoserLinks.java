eac55575f6e2f31b212478c5735eba3acf3d4275
Juliano Moser <juliano.moserjj@gmail.com>
2022-10-11 13:49:15 -0300

    Refatorando construção e inclusão de links em representation model

---
+++ src/main/java/com/moser/moserfood/api/MoserLinks.java
@@ -0,0 +1,33 @@
+package com.moser.moserfood.api;
+
+import com.moser.moserfood.api.controller.PedidoController;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+/**
+ * @author Juliano Moser
+ */
+@Component
+public class MoserLinks {
+
+    public static final TemplateVariables PAGINACAO_VARIABLES = new TemplateVariables(
+            new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+            new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+            new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM));
+
+    public Link linkToPedidos() {
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+        TemplateVariables filtroVariable = new TemplateVariables(
+                new TemplateVariable("clienteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("restauranteId", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoInicio", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("dataCriacaoFim", TemplateVariable.VariableType.REQUEST_PARAM));
+
+        return Link.of(UriTemplate.of(pedidosUrl, PAGINACAO_VARIABLES.concat(filtroVariable)), "pedidos");
+    }
+}

76c5931de69f4f360ccb64f39859721a80313823
Juliano Moser <juliano.moserjj@gmail.com>
2022-10-31 09:25:16 -0300

    Definindo a versão padrão da API quando o Media Type não é informado

--- src/main/java/com/moser/moserfood/core/web/WebConfig.java
+++ src/main/java/com/moser/moserfood/core/web/WebConfig.java
@@ -3,6 +3,7 @@ package com.moser.moserfood.core.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -23,5 +24,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public Filter shallowEtagHeader() {
         return new ShallowEtagHeaderFilter();
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(MoserMediaTypes.V2_APPLICATION_JSON);
     }
 }

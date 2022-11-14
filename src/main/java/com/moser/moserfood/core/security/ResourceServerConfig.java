b3a5e68836d9baa0095a5bf60ac4afffebb90774
Juliano Moser <juliano.moserjj@gmail.com>
2022-11-14 15:03:16 -0300

    Gerando JWT com chave simétrica (HMAC SHA-256) no Authorization Server

--- src/main/java/com/moser/moserfood/core/security/ResourceServerConfig.java
+++ src/main/java/com/moser/moserfood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.moser.moserfood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * @author Juliano Moser
@@ -19,6 +24,13 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
                 .and()
                 .cors().and()
-                .oauth2ResourceServer().opaqueToken();
+//                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder () {
+        var secretKey = new SecretKeySpec("8LZDn2fPL1piT8vC9vAzcJWcPQskjaE8jBrTfRBUDQ0".getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKey).build();
     }
 }

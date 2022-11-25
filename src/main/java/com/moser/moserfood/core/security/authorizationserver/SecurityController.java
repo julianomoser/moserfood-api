771c338fb0d8225a266b6bd03582e90c11071053
Juliano Moser <juliano.moserjj@gmail.com>
2022-11-25 14:34:09 -0300

    Customizando a página de OAuth2 Approval

--- src/main/java/com/moser/moserfood/core/security/authorizationserver/SecurityController.java
+++ src/main/java/com/moser/moserfood/core/security/authorizationserver/SecurityController.java
@@ -9,8 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SecurityController {
 
-    @GetMapping("login")
+    @GetMapping("/login")
     public String login() {
         return "pages/login";
+    }
+
+    @GetMapping("/oauth/confirm_access")
+    public String approval() {
+        return "pages/approval";
     }
 }

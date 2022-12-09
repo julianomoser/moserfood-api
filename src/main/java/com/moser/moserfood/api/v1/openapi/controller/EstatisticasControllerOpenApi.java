fcf2167c1c6afebaad108d0c7a62cd9a24c97261
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-09 10:09:48 -0300

    Descrevendo documentação de endpoint de estatísticas

--- src/main/java/com/moser/moserfood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
+++ src/main/java/com/moser/moserfood/api/v1/openapi/controller/EstatisticasControllerOpenApi.java
@@ -5,6 +5,7 @@ import com.moser.moserfood.domain.filter.VendaDiariaFilter;
 import com.moser.moserfood.domain.model.dto.VendaDiaria;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -27,7 +28,16 @@ public interface EstatisticasControllerOpenApi {
     @Operation(hidden = true)
     EstatisticaDTO estatisticas();
 
-    @Operation(summary = "Consulta estatísticas de vendas diárias", responses = {
+    @Operation(summary = "Consulta estatísticas de vendas diárias",
+            parameters = {
+                    @Parameter(in = ParameterIn.QUERY, name = "restauranteId", description = "ID do restaurante", example = "1",
+                            schema = @Schema(type = "integer")),
+                    @Parameter(in = ParameterIn.QUERY, name = "dataCriacaoInicio", description = "Data/hora inicial da criação do pedido", example = "2019-12-01T00:00:00Z", schema =
+                    @Schema(type = "string", format = "date-time")),
+                    @Parameter(in = ParameterIn.QUERY, name = "dataCriacaoFim", description = "Data/hora final da criação do pedido", example = "2019-12-02T23:59:59Z", schema =
+                    @Schema(type = "string", format = "date-time"))
+            },
+            responses = {
             @ApiResponse(responseCode = "200", content = {
                     @Content(mediaType = "application/json", array = @ArraySchema(schema =
                     @Schema(implementation = VendaDiaria.class))),
@@ -40,5 +50,7 @@ public interface EstatisticasControllerOpenApi {
                        schema = @Schema(type = "string", defaultValue = "+00:00"))
                        String timeOffset);
 
+
+    @Operation(hidden = true)
     ResponseEntity<byte[]> consultarVendasDiasriasPdf(VendaDiariaFilter filtro, String timeOffset);
 }

9f7c513d07fa44cb87b3f7c948b71ff0a9228304
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-09 09:54:54 -0300

    Corrigido Schema do FotoProdutoInput

--- src/main/java/com/moser/moserfood/api/v1/model/input/FotoProdutoInput.java
+++ src/main/java/com/moser/moserfood/api/v1/model/input/FotoProdutoInput.java
@@ -18,6 +18,7 @@ import javax.validation.constraints.NotNull;
 @Setter
 public class FotoProdutoInput {
 
+    @Schema(description = "Arquivo da foto do produto (máximo 500KB, apenas JPG e PNG)")
     @NotNull
     @FileSize(max = "500KB")
     @FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})

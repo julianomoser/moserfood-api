8e9713b8f5453fa7b0446d606f44ba67733e4065
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-17 00:39:48 -0300

    Revogando consentimentos e autorizações dos clientes

--- src/main/java/com/moser/moserfood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/moser/moserfood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -20,7 +20,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.authorization.*;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
@@ -122,7 +125,8 @@ public class AuthorizationServerConfig {
     }
 
     @Bean
-    public OAuth2AuthorizationQueryService authorizationQueryService(JdbcOperations jdbcOperations) {
-        return new JdbcOAuth2AuthorizationQueryService(jdbcOperations);
+    public OAuth2AuthorizationQueryService authorizationQueryService(JdbcOperations jdbcOperations,
+                                                                     RegisteredClientRepository registeredClientRepository) {
+        return new JdbcOAuth2AuthorizationQueryService(jdbcOperations, registeredClientRepository);
     }
 }
--- src/main/java/com/moser/moserfood/core/security/authorizationserver/OAuth2AuthorizationQueryService.java
+++ src/main/java/com/moser/moserfood/core/security/authorizationserver/OAuth2AuthorizationQueryService.java
@@ -1,5 +1,6 @@
 package com.moser.moserfood.core.security.authorizationserver;
 
+import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 
 import java.util.List;
@@ -10,4 +11,5 @@ import java.util.List;
 public interface OAuth2AuthorizationQueryService {
 
     List<RegisteredClient> listClientsWithConsent(String principalName);
+    List<OAuth2Authorization> listAuthorizations(String principalName, String clientId);
 }
